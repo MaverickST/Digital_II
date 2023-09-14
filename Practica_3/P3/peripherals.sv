1c0c286eef4111e05460d2a85c37372af3897c3b
Cristian David Araujo Araujo <51882823+Cristian-David-Araujo@users.noreply.github.com>
2023-09-14 11:49:56 -0500

    Init Pheriferal

--- Practica_3/P3/peripherals.sv
+++ Practica_3/P3/peripherals.sv
@@ -14,10 +14,29 @@ module peripherals (clk, reset, enter, inputdata,
 	output logic [6:0] disp3, disp2, disp1, disp0;
 	
 	// Internal signals and module instantiation for pulse generation
-	// WRITE HERE YOUR CODE
-
+	logic pulse;
+	peripheral_pulse pulse_generator (enter, clk, reset, pulse);
+	
 	// Process, internal signals and assign statement to control data input / output indexes and data input ready signals
-	// WRITE HERE YOUR CODE
+	logic change_position [2:0] = 3'b000;
+	//logic current_position 
+	always_ff @(posedge pulse, posedge reset) begin 
+		if(reset) begin
+			change_position <= 0;
+			inputdata_ready <= 0;
+		end else begin
+			change_position <= change_position + 1;
+		end
+	end
+	
+	always_comb
+		casez (change_position)
+		3'b?00: current_position = 0;
+		3'b?01: current_position = 8;
+		3'b?10: current_position = 16;
+		3'b?11: current_position = 24;
+		endcase
+	end
 	
 	// Internal signals and module instantiation for getting operands
 	// WRITE HERE YOUR CODE
