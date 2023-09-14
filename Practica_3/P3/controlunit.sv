98879c1f61222b9950ea6846eedf36ee02df85c6
Cristian David Araujo Araujo <51882823+Cristian-David-Araujo@users.noreply.github.com>
2023-09-14 10:35:49 -0500

    Ending of module controlunit

--- Practica_3/P3/controlunit.sv
+++ Practica_3/P3/controlunit.sv
@@ -7,21 +7,85 @@ module controlunit (clk, reset, loaddata, inputdata_ready);
 	input logic inputdata_ready;
 
 	// Internal signals for state machine
-	// WRITE HERE YOUR CODE
+	typedef enum logic {S0, S1} State;
+	State currentState, nextState;
 
 	// Process (Sequential): update currentState
-	// WRITE HERE YOUR CODE
+	always_ff @(posedge clk, posedge reset) begin
+		if (reset)
+			currentState <= S0;
+		else 
+			currentState <= nextState;
+	end
 	
 	// Process (Combinational): update nextState
-	// WRITE HERE YOUR CODE
+	always_comb begin
+		case (currentState)
+			S0:	
+				if(inputdata_ready)
+					nextState = S1;
+				else
+					nextState = currentState;
+			S1:	
+				if(inputdata_ready)
+					nextState = S1;
+				else
+					nextState = S0;
+			default:		
+				nextState = S0;
+		endcase
+	end
 
 	// Process (Combinational): update outputs 
-	// WRITE HERE YOUR CODE
+	always_comb begin
+	loaddata = 1;
+	if (currentState == S1)
+		loaddata = 0;
+	end
 endmodule
 
 // ************************** 
 // Testbench for Control Unit
 // ************************** 
 module tb_controlunit ();
-	// WRITE HERE YOUR CODE
+
+	logic  clk, reset;
+	logic  loaddata;
+	logic inputdata_ready;
+	
+	localparam fpga_freq = 8;
+	localparam delay = 20ps;
+	
+	controlunit uut (clk, reset, loaddata, inputdata_ready);
+	
+	initial begin
+		clk = 0;
+		reset = 0;
+		inputdata_ready = 0;
+		#(delay);
+		reset = 1;
+		#(delay*2);
+		inputdata_ready = 0;
+		#(delay*2);
+		inputdata_ready = 1;
+		#(delay*4);
+		inputdata_ready = 1;
+		#(delay*2);
+		inputdata_ready = 0;
+		#(delay*2);
+		reset = 0;
+		inputdata_ready = 0;
+		#(delay*2);
+		inputdata_ready = 1;
+		#(delay*4);
+		inputdata_ready = 1;
+		#(delay*4);
+		inputdata_ready = 0;
+		#(delay*4);
+		inputdata_ready = 0;
+		$stop;
+	end
+	
+	always #(delay/2) clk = ~clk;
+	
 endmodule
